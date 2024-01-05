bf8798dd588f2480a6158f3fca4ae693625c3717
Murari072001 <murari.t2001@gmail.com>
2024-01-05 19:14:33 +0530

    React

---
+++ Edupoly site/data.js
@@ -0,0 +1,167 @@
+var features=[{img:"bi bi-camera-reels",
+                            heading:"Daily Class Video Recordings",
+                        details:"Daily class videos are recorded and made available for all students for revisiting the topics."},
+                        {img:"bi bi-person-circle",
+                            heading:"Highly Experienced Trainers",
+                        details:"Our trainers are highly experienced and have ample knowledge of industry level standards."},
+                        {img:"bi bi-android2",
+                            heading:"Class Program Options",
+                        details:"We provide both online and offline classes with assured learning assistance."},
+                        {img:"bi bi-book",
+                            heading:"Several High Quality Courses",
+                        details:"All our courses are of the best quality and provide immense exposure to the industry level practices."},
+                        {img:"bi bi-patch-check",
+                            heading:"Earn a Certificate of Achievement",
+                        details:"We provide internship and training certificates on successful completion of the courses."},]
+var courses=[{  img:"https://edupoly.in/common/assets/HTML.jpg",
+                            title:"HTML",
+                            count:"4"},
+                            {  img:"https://edupoly.in/common/assets/CSS.jpg",
+                            title:"CSS",
+                            count:"4"},
+                            {  img:"https://edupoly.in/common/assets/Javascript.jpg",
+                            title:"Javascript",
+                            count:"4"},
+                            {  img:"https://edupoly.in/common/assets/Angular.jpg",
+                            title:"Angular",
+                            count:"3"},
+                            {  img:"https://edupoly.in/common/assets/React.jpg",
+                            title:"ReactJS",
+                            count:"3"},
+                            {  img:"https://edupoly.in/common/assets/NodeJS.jpg",
+                            title:"NodeJS",
+                            count:"2"},
+                            {  img:"https://edupoly.in/common/assets/Express.jpg",
+                            title:"ExpressJS",
+                            count:"2"},
+                            {  img:"https://edupoly.in/common/assets/Mongo%20db.png",
+                            title:"MongoDB",
+                            count:"2"},
+                            {  img:"https://edupoly.in/common/assets/java.png",
+                            title:"Java",
+                            count:"1"},
+                            {  img:"https://edupoly.in/common/assets/Spring.jpg",
+                            title:"Spring",
+                            count:"1"},
+                            {  img:"https://edupoly.in/common/assets/python_1.jpg",
+                            title:"Python",
+                            count:"1"},
+                            {  img:"https://edupoly.in/common/assets/django.jpg",
+                            title:"Django",
+                            count:"1"},]
+
+var trendingcourses=[
+{
+    img:"https://edupoly.in/common/assets/angular.svg",
+    title:"Frontend Web Development with Angular",
+    level:"All level",
+    duration:"3 months",
+    detils:"500+ assignments and 2 projects",
+    price:"₹20000",
+    time:"50 hours"
+},
+{
+    img:"https://edupoly.in/common/assets/react.svg",
+    title:"Frontend Web Development with ReactJS",
+    level:"All level",
+    duration:"3 months",
+    detils:"500+ assignments and 2 projects",
+    price:"₹20000",
+    time:"50 hours"
+},
+{
+    img:"https://edupoly.in/common/assets/Mean.svg",
+    title:"Full Stack Web Development with MEAN",
+    level:"Intermediate",
+    duration:"4 months",
+    detils:"500+ assignments and 2 projects",
+    price:"₹30000",
+    time:"60 hours"
+},
+{
+    img:"https://edupoly.in/common/assets/MERN.svg",
+    title:"Full Stack Web Development with MERN",
+    level:"Intermediate",
+    duration:"4 months",
+    detils:"500+ assignments and 2 projects",
+    price:"₹30000",
+    time:"60 hours"
+},
+{
+    img:"https://edupoly.in/common/assets/programming-with-java.svg",
+    title:"Java Full Stack Development Programme",
+    level:"Intermediate",
+    duration:"4 months",
+    detils:"500+ assignments and 2 projects",
+    price:"₹30000",
+    time:"60 hours"
+},
+{
+    img:"https://edupoly.in/common/assets/programming-with-python.svg",
+    title:"Python Full Stack Development Programme",
+    level:"Intermediate",
+    duration:"4 months",
+    detils:"500+ assignments and 2 projects",
+    price:"₹30000",
+    time:"60 hours"
+}] 
+
+var studentfeedback=[
+{
+    name:"Hima Shekar Ram Naidu",
+    feedback:"Edupoly is the best institute to learn with enthustiasm and definetely will learn something unexpected.Worth of it's kind."
+},
+{
+    name:"Naresh Nenavanth",
+    feedback:"My experience with EduPoly training institute was quite well and I have learned a lot.... Equal attention was given to every individual which is the best part of praveen sir."
+},
+{
+    name:"Murari",
+    feedback:`In EDUPOLY, the way of teaching and the procedure of practicing various
+    assignments &amp; tasks will provide best knowledge on Web Development.`
+},
+{
+    name:"Lakshmi Kanth",
+    feedback:`
+    The environment of institute creates for getting knowledge on web
+    technologies and provides best knowledge to the students and a lot of
+    practice assignments help us very much for students.
+    `
+},]
+
+var contacts=[
+{
+    icon:"bi bi-whatsapp",
+    bg:"bg-green",
+    title:"WhatsApp Number",
+    des:"+91 9561214818"},
+{
+    icon:"bi bi-envelope",
+    bg:"bg-orange",
+    title:"Email",
+    des:"info.edupoly@gmail.com"
+},
+{
+    icon:"bi bi-youtube",
+    bg:"bg-danger",
+    title:"Youtube",
+    des:"Praveen Classes"
+},
+{
+    icon:"bi bi-linkedin",
+    bg:"bg-primary",
+    title:"Linkedin",
+    des:"Edupoly"
+},
+{
+    icon:"bi bi-telegram",
+    bg:"bg-info",
+    title:"Telegram",
+    des:"Praveen Training"
+},
+{
+    icon:"bi bi-instagram",
+    bg:"bg-instagram",
+    title:"Instagram",
+    des:"edu.poly"
+}]
---
+++ Restaurant App/tabledetails.js
@@ -0,0 +1,65 @@
+var alltables=[
+    {
+        tableId:"table1",
+        vacancy:true,
+        currentordereditems:[],
+        previousordereditems:[],
+        seats:2
+    },
+    {
+        tableId:"table2",
+        vacancy:true,
+        currentordereditems:[],
+        previousordereditems:[],
+        seats:4
+    },
+    {
+        tableId:"table3",
+        vacancy:true,
+        currentordereditems:[],
+        previousordereditems:[],
+        seats:2
+    },
+    {
+        tableId:"table4",
+        vacancy:false,
+        currentordereditems:[],
+        previousordereditems:[],
+        seats:4
+    },
+    {
+        tableId:"table 5",
+        vacancy:true,
+        currentordereditems:[],
+        previousordereditems:[],
+        seats:6
+    },
+    {
+        tableId:"table 6",
+        vacancy:true,
+        currentordereditems:[],
+        previousordereditems:[],
+        seats:8
+    },
+    {
+        tableId:"table 7",
+        vacancy:true,
+        currentordereditems:[],
+        previousordereditems:[],
+        seats:12
+    },
+    {
+        tableId:"table 8",
+        vacancy:false,
+        currentordereditems:[],
+        previousordereditems:[],
+        seats:6
+    },
+    {
+        tableId:"table 9",
+        vacancy:true,
+        currentordereditems:[],
+        previousordereditems:[],
+        seats:4
+    },
+]
